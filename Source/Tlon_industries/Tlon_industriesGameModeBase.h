9b12c653ec6efec62267b8770da059e2ad8d76a9
Ariel Gimenez <arielgimenez_95@hotmail.com>
2022-09-30 12:05:10 -0300

    first commit

---
+++ Source/Tlon_industries/Tlon_industriesGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Tlon_industriesGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TLON_INDUSTRIES_API ATlon_industriesGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
